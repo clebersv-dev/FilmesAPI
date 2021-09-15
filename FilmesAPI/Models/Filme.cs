e6b07a28d67e1fcb03e6e3d7148ab7d28370bd38
cleber-impacta <cleberdossantos.vieira@gmail.com>
2021-09-14 23:05:43 -0300

    Primeira API em .NET 5

---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {   
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Campo titulo e obrigatorio")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "Campo diretor e obrigatorio")]
+        public string Diretor { get; set; }
+        [StringLength(30, ErrorMessage = "O Genero nao pode passar de 30 caracteres")]
+        public string Genero { get; set; }
+        [Range(1, 600, ErrorMessage = "A duração deve ter entre 1 e 600")]
+        public int Duracao { get; set; }
+    }
+}
